f450e5b078d86a6099db9a6b380badad859afa3e
succesom <omelchenko_ig@mail.ru>
2013-02-03 22:53:31 +0200

    hw2 part 2.3.13

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,12 +10,7 @@ class MoviesController < ApplicationController
 
   def index
 
-    if params[:ratings].nil? and params[:sort].nil?
-      #  @saved_sort = session[:sort]
-      # @saved_ratings = session[:ratings]
 
-        redirect_to movies_path(:sort => session[:sort], :ratings => session[:ratings])
-    end
 
      @all_ratings = Movie.all_ratings
 
