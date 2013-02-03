70e44c77c65d2a0b311babda7d5bea46fcdc71d5
succesom <omelchenko_ig@mail.ru>
2013-02-03 22:14:41 +0200

    hw2 part 2.3.7

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -47,9 +47,9 @@ class MoviesController < ApplicationController
         session[:sort] = sorted 
         session[:ratings] = params[:ratings] 
         
-        #flash.keep
+        flash.keep
         #redirect_to movies_path(@movies)
-        #redirect_to :sort => sorted, :ratings => @raits and return
+        redirect_to :sort => sorted, :ratings => @raits and return
       end
         
         #redirect_to movies_path(@movies)
